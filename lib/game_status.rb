c59b2714caf5f0fe1b5d7ac0ded92390278d15b7
Greg Morgan <gregm9999@gmail.com>
2018-10-27 21:01:34 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,16 +25,14 @@ def won?(board)
       position_1=board[win_index_1]
       position_2=board[win_index_2]
       position_3=board[win_index_3]
- 
+
       if position_1=="X" && position_2=="X" && position_3=="X"
         return win_combination
       elsif position_1=="O" && position_2=="O" && position_3=="O"
-        return win_combination
-      end   
+      end
     end
     return false
   end
-
 
 def full?(board)
   board.each_with_index do |item, index|
