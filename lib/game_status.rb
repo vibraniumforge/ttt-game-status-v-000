0f9fc405e63e520fe5c452eb9d5ba7382030d26d
Greg Morgan <gregm9999@gmail.com>
2018-10-27 21:03:45 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,10 +26,9 @@ def won?(board)
       position_2=board[win_index_2]
       position_3=board[win_index_3]
 
-      if position_1=="X" && position_2=="X" && position_3=="X"
+      if position_1==position_2 && position_2==position_3
         return win_combination
-      elsif position_1=="O" && position_2=="O" && position_3=="O"
-      end
+
     end
     return false
   end
