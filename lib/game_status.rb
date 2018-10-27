29e47ce7aae0a1753c101cdbce8cf186dcd2471f
Greg Morgan <gregm9999@gmail.com>
2018-10-27 20:44:17 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,7 +26,9 @@ def won?(board)
       position_1=board[win_index_1]
       position_2=board[win_index_2]
       position_3=board[win_index_3]
-binding.pry
+# binding.pry
+puts win_instance
+puts win_combination
       if position_1=="X" && position_2=="X" && position_3=="X"
         return win_combination
       elsif position_1=="O" && position_2=="O" && position_3=="O"
