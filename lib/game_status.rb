d645d2786795629699f03743be2347cfcf380152
Greg Morgan <gregm9999@gmail.com>
2018-10-27 21:19:47 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -54,7 +54,7 @@ def draw?(board)
 end
 
 def over?(board)
-  if won?(board)==true || draw?(board)==true || full?(board)==true
+  if won?(board) || draw?(board) || full?(board)
     return true
   end
 end
