1b32848ea7aed33dad4ca7e634e15035f9a5896f
Greg Morgan <gregm9999@gmail.com>
2018-10-27 19:52:47 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,23 +18,23 @@ WIN_COMBINATIONS=[
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
     win_combination.each do |win_instance|
-    win_index_1=win_instance[0]
-    win_index_2=win_instance[1]
-    win_index_3=win_instance[2]
+      win_index_1=win_instance[0]
+      win_index_2=win_instance[1]
+      win_index_3=win_instance[2]
 
-    position_1=board[win_index_1]
-    position_2=board[win_index_2]
-    position_3=board[win_index_3]
+      position_1=board[win_index_1]
+      position_2=board[win_index_2]
+      position_3=board[win_index_3]
 
-    if position_1=="X" && position_2=="X" && position_3=="X"
-      return win_combination
-    elsif position_1=="O" && position_2=="O" && position_3=="O"
-      return win_combination
-    else
-      return false
+      if position_1=="X" && position_2=="X" && position_3=="X"
+        return win_combination
+      elsif position_1=="O" && position_2=="O" && position_3=="O"
+        return win_combination
+      else
+        return false
+      end
     end
   end
-end
 end
 
 def full?(board)
