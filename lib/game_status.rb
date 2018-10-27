2e789a9dad3b5be2de3b637fb458a758293f4e58
Greg Morgan <gregm9999@gmail.com>
2018-10-27 19:02:45 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -48,9 +48,9 @@ def full?(board)
 end
 
 def draw?(board)
-if (won?(board)==false && full?(board)==true)
+if won?(board)==false && full?(board)==true
   return true
-elsif (won?(board==false) && full?(board)==false)
+elsif won?(board)==false && full?(board)==false
   return false
 elsif won?(board)==true
 end
